d317bc944d7aebb69323eaf5d1b56f5a82285b97
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-04 23:49:06 +0530

    refactor server.js for code clarity and formatting; enhance connection event handling

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 import { WebSocketServer } from "ws";
 
-const wss =new WebSocketServer({port:8080});
+const wss = new WebSocketServer({ port: 8080 });
 
-// connection event 
+// 0:connecting
+// 1:open(the only state where you can safely .send())
+// 2:closing
+// 3:closed
 
-wss.on('connection',(socket,request)=>{
-    const ip =request.socket.remoteAddress
-})
+// connection event
+
+wss.on("connection", (socket, request) => {
+  const ip = request.socket.remoteAddress;
+  socket.on("message", (rawData) => {
+    const message = rawData.toString();
+    console.log({ rawData });
+
+    wss.clients.forEach((client) => {
+      if (client.readyState == 1) {
+        client.send(`Server Broadcast: ${message}`);
+      }
+    });
+  });
+});
